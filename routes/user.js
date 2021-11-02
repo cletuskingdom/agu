d0cc331510a916d749b654fa402a3522f17e3261
amasoft <46465729+amasoft@users.noreply.github.com>
2021-11-02 03:29:46 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,20 @@ const { signUp, signIn, updatePassword } = require("../controllers/Authenticatio
 const router = express.Router();
 const { getAllUsers, updateUser, deleteUser, getUser, createUser } = require('./../controllers/UserController')
 
+/**
+ * @swagger 
+ * /api/v1/users/signup:
+ *   post:
+ *     tag: user signup
+ *     description: Enter your name,email,password,confirmPassword
+ *     parameters: 
+ *       - in :  body
+ *         name: userdetails
+ *         type: string
+ *     responses:
+ *       200:
+ *         description:    Success
+ */
 
 router.post('/signup', signUp)
 
